464ca5ce21c5f96aa05e85b7803da0bfd737330c
pm-sh <pmshuv975@gmail.com>
2025-02-23 00:25:10 +0300

    HW_04_Paging Refresh

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -80,10 +80,8 @@ class FeedFragment : Fragment() {
 
         lifecycleScope.launchWhenCreated {
             adapter.loadStateFlow.collectLatest {
-                binding.swiperefresh.isRefreshing = it.refresh is LoadState.Loading
-                        || it.append is LoadState.Loading
-                        || it.prepend is LoadState.Loading
-
+                binding.swiperefresh.isRefreshing =
+                    it.refresh is LoadState.Loading
             }
         }
 
